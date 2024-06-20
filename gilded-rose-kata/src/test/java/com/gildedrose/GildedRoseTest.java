4b9078293b674d41562bb2e0d0a4d4bab13e4610
Javier <jmartin@grupoloyal.com>
2024-06-20 17:02:39 +0200

    Test

--- gilded-rose-kata/src/test/java/com/gildedrose/GildedRoseTest.java
+++ gilded-rose-kata/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,10 +8,37 @@ class GildedRoseTest {
 
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[] { new Item("foo", 7, 5) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
+        assertEquals(6, app.items[0].sellIn);
+        assertEquals(4, app.items[0].quality);
+    }
+//    @Test
+//    void foo(int sellin, int quality, int sellinOut, int qualityOut) {
+//        Item[] items = new Item[] { new Item("foo", sellin, quality) };
+//        GildedRose app = new GildedRose(items);
+//        app.updateQuality();
+//        assertEquals("foo", app.items[0].name);
+//        assertEquals(sellinOut, app.items[0].sellIn);
+//        assertEquals(qualityOut, app.items[0].quality);
+//    }
+    @Test
+    void AgedBrie() {
+        Item[] items = new Item[] { new Item("Aged Brie", 0, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Aged Brie", app.items[0].name);
+    }
+    @Test
+    void Backstage() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", -1, 50) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[0].name);
+        assertEquals(-2, app.items[0].sellIn);
+        assertEquals(0, app.items[0].quality);
     }
 
 }
