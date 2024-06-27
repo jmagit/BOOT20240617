9a418d28097f3772d511100ab6282a0b46384f69
Javier <jmartin@grupoloyal.com>
2024-06-27 13:02:52 +0200

    personaDBItemWriter

--- lotes-java/src/main/java/com/example/batch/PersonasBatchConfiguration.java
+++ lotes-java/src/main/java/com/example/batch/PersonasBatchConfiguration.java
@@ -1,33 +1,53 @@
 package com.example.batch;
 
+import javax.sql.DataSource;
+
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.sql.init.dependency.DependsOnDatabaseInitialization;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import com.example.models.Persona;
 import com.example.models.PersonaDTO;
 
 @Configuration
 public class PersonasBatchConfiguration {
 	@Autowired
 	JobRepository jobRepository;
-	
+
 	@Autowired
 	PlatformTransactionManager transactionManager;
 
 	public FlatFileItemReader<PersonaDTO> personaCSVItemReader(String fname) {
 		return new FlatFileItemReaderBuilder<PersonaDTO>().name("personaCSVItemReader")
-				.resource(new FileSystemResource(fname))
-				.linesToSkip(1)
-				.delimited()
+				.resource(new FileSystemResource(fname)).linesToSkip(1).delimited()
 				.names(new String[] { "id", "nombre", "apellidos", "correo", "sexo", "ip" })
-				.fieldSetMapper(new BeanWrapperFieldSetMapper<PersonaDTO>() { {
+				.fieldSetMapper(new BeanWrapperFieldSetMapper<PersonaDTO>() {
+					{
 						setTargetType(PersonaDTO.class);
-					}})
+					}
+				}).build();
+	}
+
+	@Autowired
+	public PersonaItemProcessor personaItemProcessor;
+
+	@Bean
+	@DependsOnDatabaseInitialization
+	public JdbcBatchItemWriter<Persona> personaDBItemWriter(DataSource dataSource) {
+		return new JdbcBatchItemWriterBuilder<Persona>()
+				.itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+				.sql("INSERT INTO personas VALUES (:id,:nombre,:correo,:ip)")
+				.dataSource(dataSource)
 				.build();
 	}
 
