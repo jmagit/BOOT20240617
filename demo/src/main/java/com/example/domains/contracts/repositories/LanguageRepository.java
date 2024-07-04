07f13439595705c4704bacb28d7eed89905e5c6f
Javier <jmartin@grupoloyal.com>
2024-07-04 10:57:09 +0200

    referencias openapi

--- demo/src/main/java/com/example/domains/contracts/repositories/LanguageRepository.java
+++ demo/src/main/java/com/example/domains/contracts/repositories/LanguageRepository.java
@@ -2,10 +2,13 @@ package com.example.domains.contracts.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.example.domains.entities.Language;
 
 @RepositoryRestResource(path="idiomas", itemResourceRel="idioma", collectionResourceRel="idiomas")
 public interface LanguageRepository extends JpaRepository<Language, Integer> {
-
+	@Override
+	@RestResource(exported = false)
+	void deleteById(Integer id);
 }
