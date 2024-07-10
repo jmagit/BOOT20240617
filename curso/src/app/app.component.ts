1c59b246accce00b99719c2025b6f0e05e6d7d59
Javier <jmartin@grupoloyal.com>
2024-07-10 11:41:04 +0200

    @my/core

--- curso/src/app/app.component.ts
+++ curso/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SecurityModule } from './security';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SecurityModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
